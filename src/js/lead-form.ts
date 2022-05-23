313076871e702214ff3c3f6ba3494087db926359
Rodrigo Passos <rodrigopassos@mac.com>
2022-05-23 16:00:44 -0400

    add formbucket submission handling

--- src/js/lead-form.ts
+++ src/js/lead-form.ts
@@ -1,6 +1,8 @@
 const leadForm = (() => {
 
-  let _form: HTMLFormElement|null = null
+  const RECAPTCHA_CLASS = 'g-recaptcha'
+
+  let _form: HTMLFormElement | null = null
 
   const _handleSuccess = () => {
 
@@ -18,11 +20,60 @@ const leadForm = (() => {
 
   }
 
-  const _handleSubmit = (e: Event) => {
+  const _renderReCaptchaError = (error?: Error) => {
+    const recaptcha = document.querySelector(`.${RECAPTCHA_CLASS}:first-child > div`)
+
+    if (recaptcha && error !== undefined) return recaptcha?.classList.add('border','border-emphasis', 'rounded')
+    return recaptcha?.classList.remove('border','border-emphasis', 'rounded')
+  }
+
+  const _handleError = (error: Error) => {
+    switch (error.message) {
+      case "grecaptcha is not defined":
+        _renderReCaptchaError(error)
+        break;
+      case "grecaptcha is not completed":
+        _renderReCaptchaError(error)
+        break;
+
+      default:
+        console.error(error);
+
+        break;
+    }
+  }
+
+  const _handleSubmit = async (e: Event) => {
 
     e.preventDefault()
 
-    _handleSuccess()
+    const formData = new FormData(e.target as HTMLFormElement)
+
+    if (_form && _form?.action.length > 0) {
+      try {
+
+        if (grecaptcha === undefined) {
+          throw new Error('grecaptcha is not defined')
+        }
+
+        if (grecaptcha.getResponse() == "") {
+          throw new Error('grecaptcha is not completed')
+        }
+
+        _renderReCaptchaError()
+
+        const response = await fetch(_form?.action, {
+          method: 'POST',
+          body: formData,
+        })
+
+        if (response.ok) {
+          _handleSuccess()
+        }
+      } catch (error) {
+        _handleError(error as Error)
+      }
+    }
 
   }
 
