26fe58d4e1efe70c4a685c35b608344923ba6e50
Nathan <novadrake76@gmail.com>
2022-05-24 20:06:50 -0300

    feat: mobile view completed

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,1 +1,19 @@
-console.log('Hello World!!!');
+console.log('Hello World!!!')
+
+const buttonMessage = document.getElementById('messageCard')
+const messageSent = document.getElementById('messageSent')
+
+if (buttonMessage != null) {
+  buttonMessage.addEventListener('click', sendMessage)
+}
+
+function sendMessage() {
+  if (buttonMessage != null && messageSent != null) {
+    if (buttonMessage.style.display === 'none') {
+      buttonMessage.style.display = 'flex'
+    } else {
+      messageSent.style.display = 'flex'
+      buttonMessage.style.display = 'none'
+    }
+  }
+}
