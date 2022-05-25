ff8101d49acab4092581db7f2e5471461eaa6e5e
NovaDrake76 <novadrake76@gmail.com>
2022-05-25 15:43:00 -0300

    feat: desktop view completed.

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,19 +1,17 @@
 console.log('Hello World!!!')
 
-const buttonMessage = document.getElementById('messageCard')
+const messageCard = document.getElementById('messageCard')
+const buttonMessage = document.getElementById('btnCard')
 const messageSent = document.getElementById('messageSent')
 
-if (buttonMessage != null) {
-  buttonMessage.addEventListener('click', sendMessage)
-}
+  buttonMessage!.addEventListener('click', sendMessage)
 
+//
 function sendMessage() {
-  if (buttonMessage != null && messageSent != null) {
-    if (buttonMessage.style.display === 'none') {
-      buttonMessage.style.display = 'flex'
+    if (messageCard!.style.display === 'none') {
+      messageCard!.style.display = 'flex'
     } else {
-      messageSent.style.display = 'flex'
-      buttonMessage.style.display = 'none'
-    }
+      messageSent!.style.display = 'flex'
+      messageCard!.style.display = 'none'
   }
 }
